b8ce82d918eddd980a1949421307627f0ffeea00
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-06-03 12:12:58 -0400

    feat: implement package show status to list PR/issues

--- src/scikit_package/cli/show/status.py
+++ src/scikit_package/cli/show/status.py
@@ -1,26 +1,16 @@
 import os
-import subprocess
-
-import click
-
-from scikit_package.utils import api, auth, io
+from scikit_package.utils import io, file
 from scikit_package.utils.shell import run
-from scikit_package.utils import file
 
 
 def main(args):
     dev_path = io.get_config_value("dev_path")
-    # Get folders below the dev path
     folder_names = file.list_folders(dev_path)
-    # Loop through each folder and list the issues and PRs with run
-    for folder in folder_names:
-        folder_path = os.path.join(dev_path, folder)
-        print(f"\nFolder: {folder}")
-        run(
-            f"gh issue list",
-            cwd=folder_path,
-        )
-        run(
-            f"gh pr list",
-            cwd=folder_path,
-        )
+    folder_count = len(folder_names)
+    for i, name in enumerate(folder_names, start=1):
+        folder_path = os.path.join(dev_path, name)
+        print("\n" + "=" * 40)
+        print(f"({i}/{folder_count}) {name}")
+        print("" + "=" * 40)
+        run(f"gh issue list", cwd=folder_path)
+        run(f"gh pr list", cwd=folder_path)
